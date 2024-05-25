76186baac1042d87ab567a12234966d326bc8a03
Manuel Parra <81319513+Hvnt3rK3ys@users.noreply.github.com>
2024-05-25 07:39:34 -0500

    UPDATE: host

--- TallerSQS/TreeSQSApi/main.py
+++ TallerSQS/TreeSQSApi/main.py
@@ -241,4 +241,4 @@ async def desencolar(cantidad_enviada: dict):
 
 # Punto de entrada para iniciar la aplicación
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
